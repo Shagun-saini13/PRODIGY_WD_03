df34cc26dfa68b7bd6121c66a5e13b065fc975d9
Shagun-saini13 <sainishagun001@gmail.com>
2025-07-07 11:01:34 +0530

    added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let resetBtn = document.querySelectorAll("#reset-btn");
 
 let turnO= true; //playerX, playerO
 
-const winPatterns = {
+const winPatterns = {[
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -12,4 +12,18 @@ const winPatterns = {
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8]
-}
+]}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", ()=> {
+        console.log("box was clicked");
+        if(turnO) { //player O
+            box.innerText = "O";
+            turnO = false;
+        }
+        else { //Player X
+            box.innerText = "X";
+            turnO = true;
+        }
+    });
+});
